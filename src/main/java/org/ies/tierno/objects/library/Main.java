186d202e6d62be279a6225660768074c1eb84349
DavidLopez <david.lopez115@educa.madrid.org>
2025-11-13 10:04:44 +0100

    first commit

---
+++ src/main/java/org/ies/tierno/objects/library/Main.java
@@ -0,0 +1,21 @@
+package org.ies.tierno.objects.library;
+
+import org.ies.tierno.objects.library.model.Library;
+import org.ies.tierno.objects.library.readers.AuthorReader;
+import org.ies.tierno.objects.library.readers.BookReader;
+import org.ies.tierno.objects.library.readers.LibraryReader;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        var authorReader = new AuthorReader(scanner);
+        var bookReader = new BookReader(scanner, authorReader);
+        var libraryReader = new LibraryReader(scanner, bookReader);
+
+        Library library = libraryReader.read();
+
+        System.out.println(library);
+    }
+}
